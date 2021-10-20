792e609305aca32f34a74d24255280775137012b
Qais Hamarneh <qais.hamarneh@tum.de>
2021-10-21 01:21:35 +0200

    Implemented parsers from and to a grid

--- grid.py
+++ grid.py
@@ -1,23 +1,25 @@
 from enum import Enum
+import numpy as np
 
 
 class CellType(Enum):
     EMPTY = 0
-    OBSTACLE = 1
-    TARGET = 2
-    OCCUPIED = 3
+    PEDESTRIAN = 1
+    OBSTACLE = 2
+    TARGET = 3
 
 
 class Cell:
     """
     Cell class represents one cell in the grid. A cell can be a target, an obstacle or empty
     """
+
     def __init__(self, row, col, cell_type=CellType.EMPTY):
         """
         Initiate the cell with the given position to an empty cell
         :param row: represents the cell position in the x-axis
         :param col: represents the cell position in the y-axis
-        :param type: the type of the cell
+        :param cell_type: the type of the cell
         """
         super().__init__()
         self.row = row
@@ -28,7 +30,8 @@ class Cell:
 
 class Pedestrian:
     """
-    Pedestrian class represents a 2D array of cells.
+    A Pedestrian has a unique cell and a unique id.
+    The cell is updated after every step based on a utility function.
     """
     _id_counter = 0
 
@@ -47,14 +50,15 @@ class Pedestrian:
         Checks that the cell where the pedestrian is standing is occupied.
         :return: true or false
         """
-        return self.cell.cell_type == CellType.OCCUPIED
+        return self.cell.cell_type == CellType.PEDESTRIAN
 
 
 class Grid:
     """
     Grid class represents a 2D array of cells.
     """
-    def __init__(self, rows, cols):
+
+    def __init__(self, rows, cols, cells=None):
         """
         Set up basic attributes for the GUI object
         :param rows: row size of grid
@@ -65,27 +69,29 @@ class Grid:
         self.cols = cols
         self.pedestrians = []
         self.targets = []
-        self.cells = [[Cell(row, column) for column in range(cols)] for row in range(rows)]
+        if cells is None:
+            self.cells = np.array([[Cell(row, column) for column in range(cols)] for row in range(rows)])
+        else:
+            self.cells = cells
 
     def is_valid(self):
         """
         Checks if all cells in the grid are valid cells and that there are no illegal overlap
-        :return: true or false
+        :return: true or false with a error message
         """
         if any(not ped.is_valid() for ped in self.pedestrians):
-            print(f"Some pedestrians are standing on cells with invalid types")
-            return False
+            # print(f"Some pedestrians are standing on cells with invalid types")
+            return False, "Some pedestrians are standing on cells with invalid types"
         if any(not tar.cell_type == CellType.TARGET for tar in self.targets):
-            print(f"Some targets cells have the wrong type")
-            return False
+            # print(f"Some targets cells have the wrong type")
+            return False, f"Some targets cells have the wrong type"
 
         for i in range(len(self.pedestrians)):
-            for j in range(i, len(self.pedestrians)):
+            for j in range(i + 1, len(self.pedestrians)):
                 if self.pedestrians[i].cell.row == self.pedestrians[j].cell.row and \
-                   self.pedestrians[i].cell.col == self.pedestrians[j].cell.col:
-                    print(f"pedestrians {self.pedestrians[i].id} and {self.pedestrians[j].id} "
-                          f"are standing on the same cell")
-                    return False
-        return True
-
-
+                        self.pedestrians[i].cell.col == self.pedestrians[j].cell.col:
+                    # print(f"pedestrians {self.pedestrians[i].id} and {self.pedestrians[j].id} "
+                    #       f"are standing on the same cell")
+                    return False, f"pedestrians {self.pedestrians[i].id} and {self.pedestrians[j].id} " \
+                                  f"are standing on the same cell"
+        return True, "The grid is valid"
