1. Fixed task_4 so all pedestrians reach at the same time 2. Added cell scaling so that speed can be scaled properly. 3. updated meeting notes with tasks to be done

grid_structure.py
@@ -464,9 +464,9 @@ class Grid:
         :return: Returns the numpy array that contain all the dijkstra costs for each cell.
         """
         dijkstra_array = []
-        for row_ind, row in self.cells:
+        for row in self.cells:
             d_row = []
-            for col_ind, cell in row:
+            for cell in row:
                 d_row.append(cell.dijkstra_cost)
             dijkstra_array.append(d_row)
         dijkstra_array = np.array(dijkstra_array)

utilities.py
@@ -58,8 +58,10 @@ def scenario_loader():
         scenario = np.array(list(csv.reader(csvfile))).astype(int)
     # Display the inital state of the scenario
     print("Initial state of the loaded scenario:")
+    cell_size_meters = float(input("Please select the size of each cell in meters. For the given scenarios "
+                                 "please refer to the discretization table in the notebook or in the README.md file."))
     # visualize_state(scenario)
-    return parser_array2obj(scenario)
+    return parser_array2obj(scenario), cell_size_meters
 
 
 def scenario_builder():
@@ -78,5 +80,8 @@ def scenario_builder():
 
     """
     # Get size of grid from user
+    cell_size_meters = float(input("Please select the size of each cell in meters. For the given scenarios "
+                                 "please refer to the discretization table in the notebook or in the README.md file."))
+
     rows, cols = tuple([eval(x) for x in input("Enter the size of the grid in the format: rows, columns: ").split(',')])
-    return Grid(rows, cols)
+    return Grid(rows, cols), cell_size_meters